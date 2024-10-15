eec47e527733ae10f3dbc92fcfea4419f22686b7
palaganisubramanyam <subbusubramanyam99553@gmail.com>
2024-10-15 10:24:26 +0530

    Create q5.py

---
+++ q5.py
@@ -0,0 +1,12 @@
+'''
+def sum_of_array_elements():
+    n = int(input("Enter the number of elements in the array: "))
+    total_sum = 0
+    for _ in range(n):
+        element = int(input("Enter an integer: "))
+        total_sum += element
+    print(total_sum)
+sum_of_array_elements()
+'''
+
+11
