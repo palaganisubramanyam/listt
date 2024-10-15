a666dc0c9069445746194a2565fd70e4355e75e3
palaganisubramanyam <subbusubramanyam99553@gmail.com>
2024-10-15 10:14:01 +0530

    Create q1.py

---
+++ q1.py
@@ -0,0 +1,12 @@
+'''
+def create_and_display_list():
+    n = int(input("Enter the size of the list: "))
+    integer_list = []
+    for _ in range(n):
+        element = int(input("Enter an integer: "))
+        integer_list.append(element)
+    print(integer_list)
+create_and_display_list()
+'''
+
+[1, 2, 3, 4]
