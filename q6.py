c7f4f02cd17b2be21025dfeef150e25b3eec24b0
palaganisubramanyam <subbusubramanyam99553@gmail.com>
2024-10-15 10:26:05 +0530

    Create q6.py

---
+++ q6.py
@@ -0,0 +1,12 @@
+'''
+def find_smallest_number():
+    n = int(input("Enter the size of the list: "))
+    integer_list = []
+        element = int(input("Enter an integer: "))
+        integer_list.append(element)
+    smallest_number = min(integer_list)
+    print(smallest_number)
+find_smallest_number()
+'''
+
+1
