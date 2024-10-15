3ad9c442658499b566173f95fb0dcac4686420bc
palaganisubramanyam <subbusubramanyam99553@gmail.com>
2024-10-15 10:20:34 +0530

    Create q3.py

---
+++ q3.py
@@ -0,0 +1,13 @@
+'''
+def find_largest_number():
+    n = int(input("Enter the size of the list: "))
+    integer_list = []
+    for _ in range(n):
+        element = int(input("Enter an integer: "))
+        integer_list.append(element)
+    largest_number = max(integer_list)
+    print(largest_number)
+find_largest_number()
+'''
+
+6
