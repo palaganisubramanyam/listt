9da41c2c8750bb465e0d286a79672a547611a6b0
palaganisubramanyam <subbusubramanyam99553@gmail.com>
2024-10-15 10:31:33 +0530

    Create q9.py

---
+++ q9.py
@@ -0,0 +1,11 @@
+'''
+def print_reverse_list():
+    elements = input("Enter the list elements: ").split()
+    print(" ".join(reversed(elements)))
+print_reverse_list()
+'''
+
+Sample Input:
+10 20 30 40 50
+Sample Output:
+50 40 30 20 10 
